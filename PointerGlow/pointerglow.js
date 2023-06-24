7173ebffca03b58612cfe87dd7147d542e0084bd
bedukumar <99368814+bedukumar@users.noreply.github.com>
2023-06-24 23:04:56 +0530

    Add files via upload

---
+++ PointerGlow/pointerglow.js
@@ -0,0 +1,13 @@
+const handleOnMouse = (e) => {
+  console.log(e);
+  const { currentTarget: target } = e;
+
+  const rect = target.getBoundingClientRect(),
+    x = e.clientX - rect.left,
+    y = e.clientY - rect.top;
+  target.style.setProperty("--mouse-x", `${x}px`);
+  target.style.setProperty("--mouse-y", `${y}px`);
+};
+for (const card of document.querySelectorAll(".lowerrec")) {
+  card.onmousemove = (e) => handleOnMouse(e);
+}
